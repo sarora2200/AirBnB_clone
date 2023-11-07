417a2ad4d53546127be3ccd83ca8b6ab5dbcf51a
sarora2200 <sarora2200@gmail.com>
2023-11-07 00:04:59 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ import uuid
 import models
 from datetime import datetime
 
+
 class BaseModel:
 
     """class that defines all common attributes/methods for other classes"""
@@ -35,10 +36,39 @@ class BaseModel:
             self.updated_at = datetime.now()
             models.storage.new(self)
 
-
     def __str__(self):
         """return string representation of of basemodel instance"""
-       className = self.__class__.__name__
-        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
 
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
+    def save(self):
+        """Updates the attribute updated_at with the current datetime"""
+
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+
+        My_dict = self.__dict__.copy()
+        My_dict['updated_at'] = self.updated_at.isoformat()
+        My_dict['created_at'] = self.created_at.isoformat()
+        My_dict['__class__'] = self.__class__.__name__
+        # My_dict = dict()
+        # My_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if key in ('created_at', 'updated_at'):
+        #        My_dict[key] = value.isoformat()
+        #    else:
+        #        My_dict[key] = value
+        return My_dict
+        # My_dict = dict()
+        # My_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if type(value) is datetime:
+        #        My_dict[key] = value.isoformat()
+        #    else:
+        #        My_dict[key] = value
+        # return My_dict
